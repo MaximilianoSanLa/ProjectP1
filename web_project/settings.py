026838bc06658e17fdba97b08f8528e12ae7c6bd
MaximilianoSanLa <maximiliano.sanchez.lara@gmail.com>
2024-03-10 22:08:53 -0400

    Hopefull Fix

--- web_project/settings.py
+++ web_project/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
-    'client',
     'user',
+    'client',
+    'Client_User',
 ]
 
 MIDDLEWARE = [
