7410cc5ba2335328a0a618b470bbf0f0be4172cd
JPZU <juanpablozp@icloud.com>
2024-03-14 10:05:58 -0500

    Complete homeClient

--- client/views.py
+++ client/views.py
@@ -1,16 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from Client_User.models import Report,File,Appointment,Client,Vet,Log_in, Pets
+from django.shortcuts import render
+from Client_User.models import Report,File,Appointment,Client,Vet,Log_in
 from django.shortcuts import render, redirect
-import pdb
 
-def homeClient(request, id_cliente):
-
-    client = Client.objects.get(pk=id_cliente)
-    pets = Pets.objects.filter(client_id=client)
-    print(pets)
-    # pdb.set_trace()
-    return render(request, 'homeClient.html', {'pets': pets, 'client': client})
+def homeClient(request):
+    return render(request, 'homeClient.html')
 
 def registerPet(request):
     return render(request, 'registerPet.html')
